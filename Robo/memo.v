30893adda9632483ca168b0f42a1859b7ac12855
matsouto <matsouto55@gmail.com>
2024-08-15 17:12:43 -0300

    add memory and top modules and testbenches

---
+++ Robo/memo.v
@@ -0,0 +1,252 @@
+module memo (
+    input clock, 
+    input reset,
+    input avancar,
+    input girar,
+    input remover,
+    output reg head_out,
+    output reg left_out,
+    output reg under_out,
+    output reg barrier_out
+);
+
+// Par?metros para as celulas
+parameter WALL = 3'h0,
+            PATH = 3'h1,
+            BARRIER3 = 3'h2,
+            BARRIER6 = 3'h3,
+            BARRIER9 = 3'h4,
+            BLACK = 3'h7;
+
+// Par?metros para a orienta??o do rob?
+parameter NORTH = 2'b00, 
+          WEST = 2'b01, 
+          SOUTH  = 2'b10, 
+          EAST  = 2'b11;
+
+// Matrix dimensions: 10x20
+reg [2:0] map [0:9][0:19];  // 0-based indexing para 10x20
+reg [3:0] robo_row;    // Coluna da posição do robô (4 bits para 0-9)
+reg [4:0] robo_col;    // Coluna da posição do robô (5 bits para 0-19)
+reg [1:0] robo_orientacao;  // Orientação do robô
+
+reg [1:0] barrier_counter; // Contador de clock para BARRIER
+
+initial begin
+    // Inicializa o mapa na matriz
+    $readmemh("map.hex", map); // L? dados bin?rios de um arquivo
+
+    robo_row = 4'b1001; // Posição de linha inicial (9)
+    robo_col = 5'b00000; // Posição de coluna inicial (0)
+    robo_orientacao = NORTH; // Orientação inicial (North)
+    barrier_counter = 4'b0000; // Contador de BARRIER zerado
+end
+
+// L?gica da orienta??o	e movimenta??o
+always @(posedge clock or posedge reset) begin 
+    if (reset) begin
+        // Reset the robot's position and orientation
+        robo_row <= 4'b1001;
+        robo_col <= 5'b00000;
+        robo_orientacao <= NORTH;
+        barrier_counter <= 4'b0000;
+    end 
+    else begin
+        // Lógica para girar
+        if (girar) begin
+            if (robo_orientacao == EAST) // Retorna para o NORTH
+                robo_orientacao <= NORTH; 
+            else
+                robo_orientacao <= robo_orientacao + 2'b01; // Rotaciona no sentido anti-hor?rio
+        end
+        // Lógica para avançar
+        else if (avancar) begin
+            case (robo_orientacao)
+                NORTH:
+                    robo_row <= robo_row - 1;
+                WEST:
+                    robo_col <= robo_col - 1;
+                SOUTH:
+                    robo_row <= robo_row + 1;
+                EAST:
+                    robo_col <= robo_col + 1;
+            endcase
+        end
+        // Lógica para remover
+        else if (remover) begin
+            barrier_counter <= barrier_counter + 1;
+
+            if (barrier_counter == 2'b10) begin // Conta até 2
+                case (robo_orientacao)
+                    NORTH:
+                        case(map[robo_row - 1][robo_col])
+                            BARRIER9: 
+                                map[robo_row - 1][robo_col] <= BARRIER6;
+                            BARRIER6: 
+                                map[robo_row - 1][robo_col] <= BARRIER3;
+                            BARRIER3: 
+                                map[robo_row - 1][robo_col] <= PATH;
+                        endcase
+                    WEST:
+                        case(map[robo_row][robo_col - 1])
+                            BARRIER9: 
+                                map[robo_row][robo_col - 1] <= BARRIER6;
+                            BARRIER6: 
+                                map[robo_row][robo_col - 1] <= BARRIER3;
+                            BARRIER3: 
+                                map[robo_row][robo_col - 1] <= PATH;
+                        endcase
+                    SOUTH:
+                        case(map[robo_row + 1][robo_col])
+                            BARRIER9: 
+                                map[robo_row + 1][robo_col] <= BARRIER6;
+                            BARRIER6: 
+                                map[robo_row + 1][robo_col] <= BARRIER3;
+                            BARRIER3: 
+                                map[robo_row + 1][robo_col] <= PATH;
+                        endcase
+                    EAST:
+                        case(map[robo_row][robo_col + 1])
+                            BARRIER9: 
+                                map[robo_row][robo_col + 1] <= BARRIER6;
+                            BARRIER6: 
+                                map[robo_row][robo_col + 1] <= BARRIER3;
+                            BARRIER3: 
+                                map[robo_row][robo_col + 1] <= PATH;
+                        endcase
+                endcase
+                barrier_counter <= 2'b00; // Reseta o contador
+            end
+        end
+    end
+end
+
+// Logica combinacional das saidas
+always @(*) begin
+    case (robo_orientacao)
+        NORTH: begin
+            // L?gica do head_out
+            if (robo_row == 0 || map[robo_row - 1][robo_col] == WALL) 
+                head_out = 1;
+            else 
+                head_out = 0;
+
+            // L?gica do left_out
+            if (robo_col == 0 || map[robo_row][robo_col - 1] == WALL) 
+                left_out = 1;
+            else 
+                left_out = 0;
+
+            // L?gica do under_out
+            if (map[robo_row][robo_col] == BLACK)
+                under_out = 1;
+            else
+                under_out = 0;
+
+            // L?gica do barrier_out
+            if (robo_row != 0 && 
+                (map[robo_row - 1][robo_col] == BARRIER3 || 
+                map[robo_row - 1][robo_col] == BARRIER6 || 
+                map[robo_row - 1][robo_col] == BARRIER9)) begin
+                barrier_out = 1;
+	    end
+            else
+                barrier_out = 0;
+        end
+        WEST: begin
+            // L?gica do head_out
+            if (robo_col == 0 || map[robo_row][robo_col - 1] == WALL) 
+                head_out = 1;
+            else 
+                head_out = 0;
+
+            // L?gica do left_out
+            if (robo_row == 9 || map[robo_row + 1][robo_col] == WALL) 
+                left_out = 1;
+            else 
+                left_out = 0;
+
+            // L?gica do under_out
+            if (map[robo_row][robo_col] == BLACK)
+                under_out = 1;
+            else
+                under_out = 0;
+
+            // L?gica do barrier_out
+            if (robo_col != 0 && 
+                (map[robo_row][robo_col - 1] == BARRIER3 || 
+                map[robo_row][robo_col - 1] == BARRIER6 || 
+                map[robo_row][robo_col - 1] == BARRIER9)) begin
+                barrier_out = 1;
+	    end
+            else
+                barrier_out = 0;
+        end
+        SOUTH: begin
+            // L?gica do head_out
+            if (robo_row == 9 || map[robo_row + 1][robo_col] == WALL) 
+                head_out = 1;
+            else 
+                head_out = 0;
+
+            // L?gica do left_out
+            if (robo_col == 19 || map[robo_row][robo_col + 1] == WALL) 
+                left_out = 1;
+            else 
+                left_out = 0;
+
+            // L?gica do under_out
+            if (map[robo_row][robo_col] == BLACK)
+                under_out = 1;
+            else
+                under_out = 0;
+
+            // L?gica do barrier_out
+            if (robo_row != 9 && 
+                (map[robo_row + 1][robo_col] == BARRIER3 || 
+                map[robo_row + 1][robo_col] == BARRIER6 || 
+                map[robo_row + 1][robo_col] == BARRIER9)) begin
+                barrier_out = 1;
+	    end
+            else
+                barrier_out = 0;
+        end
+        EAST: begin
+            // L?gica do head_out
+            if (robo_col == 19 || map[robo_row][robo_col + 1] == WALL) 
+                head_out = 1;
+            else 
+                head_out = 0;
+
+            // L?gica do left_out
+            if (robo_row == 0 || map[robo_row - 1][robo_col] == WALL) 
+                left_out = 1;
+            else 
+                left_out = 0;
+
+            // L?gica do under_out
+            if (map[robo_row][robo_col] == BLACK)
+                under_out = 1;
+            else
+                under_out = 0;
+
+            // L?gica do barrier_out
+            if (robo_col != 19 && 
+                (map[robo_row][robo_col + 1] == BARRIER3 || 
+                map[robo_row][robo_col + 1] == BARRIER6 || 
+                map[robo_row][robo_col + 1] == BARRIER9)) begin
+                barrier_out = 1;
+	    end
+            else
+                barrier_out = 0;
+        end
+        default: begin
+            head_out = 0;
+            left_out = 0;
+        end
+    endcase
+end
+
+endmodule
+
+
---
+++ Robo/robo.v
@@ -0,0 +1,299 @@
+module robo
+
+  (
+    input wire clock,
+    input wire reset,
+    input wire head,
+    input wire left,
+    input wire under,
+    input wire barrier,
+    output reg avancar,
+    output reg girar,
+    output reg remover
+  );
+  parameter WIDTH = 3, // Lista de parâmetros para os estados
+            Procurando_Muro = 0,
+            Rotacionando = 1,
+            Acompanhando_Muro = 2,
+            Iniciando = 3,
+            Removendo = 4,
+            Standby = 5;
+
+  reg [WIDTH-1:0] Estado_Atual, Proximo_Estado;  // Registradores de estado
+
+
+  always @(posedge clock, posedge reset)
+  begin
+    if (reset == 1'b1)
+    begin
+      Estado_Atual <= Iniciando;
+      Proximo_Estado <= Iniciando;
+    end
+    else
+    begin
+      Estado_Atual <= Proximo_Estado;
+    end
+  end
+
+  always @(negedge clock)
+  begin
+    case(Estado_Atual)
+      Procurando_Muro:
+      begin
+        casez({head,left,under,barrier})
+          4'b0100:
+          begin
+            Proximo_Estado <= Acompanhando_Muro;
+          end
+          4'b??1?:
+          begin
+            Proximo_Estado <= Standby;
+          end
+          4'b??01:
+          begin
+            Proximo_Estado <= Removendo;
+          end
+          4'b1?00:
+          begin
+            Proximo_Estado <= Rotacionando;
+          end
+          default:
+          begin
+            Proximo_Estado <= Procurando_Muro;
+          end
+        endcase
+      end
+      Rotacionando:
+      begin
+        casez({head,left,under,barrier})
+          4'b0100:
+          begin
+            Proximo_Estado <= Acompanhando_Muro;
+          end
+          4'b??1?:
+          begin
+            Proximo_Estado <= Standby;
+          end
+          4'b??01:
+          begin
+            Proximo_Estado <= Removendo;
+          end
+          default:
+          begin
+            Proximo_Estado <= Rotacionando;
+          end
+        endcase
+      end
+      Acompanhando_Muro:
+      begin
+        casez({head,left,under,barrier})
+          4'b1000 , 4'b0000:
+          begin
+            Proximo_Estado <= Procurando_Muro;
+          end
+          4'b1100:
+          begin
+            Proximo_Estado <= Rotacionando;
+          end
+          4'b??1?:
+          begin
+            Proximo_Estado <= Standby;
+          end
+          4'b??01:
+          begin
+            Proximo_Estado <= Removendo;
+          end
+          default:
+          begin
+            Proximo_Estado <= Acompanhando_Muro;
+          end
+        endcase
+      end
+      Iniciando:
+      begin
+        casez({head,left,under,barrier})
+          4'b??10:
+          begin
+            Proximo_Estado <= Iniciando;
+          end
+          4'b0100:
+          begin
+            Proximo_Estado <= Acompanhando_Muro;
+          end
+          4'b0000:
+          begin
+            Proximo_Estado <= Procurando_Muro;
+          end
+          4'b1100:
+          begin
+            Proximo_Estado <= Rotacionando;
+          end
+          4'b1000:
+          begin
+            Proximo_Estado <= Procurando_Muro;
+          end
+          4'b???1:
+          begin
+            Proximo_Estado <= Removendo;
+          end
+        endcase
+      end
+      Standby:
+      begin
+        Proximo_Estado <= Standby;
+      end
+      Removendo:
+      begin
+        casez ({head,left,under,barrier})
+          4'b???1:
+            Proximo_Estado <= Removendo ;
+          4'b?1?0:
+            Proximo_Estado <= Acompanhando_Muro ;
+          4'b?0?0:
+            Proximo_Estado <= Procurando_Muro ;
+        endcase
+      end
+    endcase
+  end
+
+  always @(Estado_Atual,head,left,under,barrier)
+  begin
+    case(Estado_Atual)
+
+      Procurando_Muro:
+      begin
+        casez({head,left,under,barrier})
+          4'b0?00:
+          begin
+            avancar = 1'b1;
+            girar = 1'b0;
+            remover = 1'b0;
+          end
+          4'b??1?:
+          begin
+            avancar = 1'b0;
+            girar = 1'b0;
+            remover = 1'b0;
+          end
+          4'b0?01:
+          begin
+            avancar = 1'b0;
+            girar = 1'b0;
+            remover = 1'b1;
+          end
+          4'b1?00:
+          begin
+            avancar = 1'b0;
+            girar = 1'b1;
+            remover = 1'b0;
+          end
+        endcase
+      end
+      Rotacionando:
+      begin
+        casez({head,left,under,barrier})
+          4'b0100:
+          begin
+            avancar = 1'b1;
+            girar = 1'b0;
+            remover = 1'b0;
+          end
+          4'b??1?:
+          begin
+            avancar = 1'b0;
+            girar = 1'b0;
+            remover = 1'b0;
+          end
+          4'b0?01:
+          begin
+            avancar = 1'b0;
+            girar = 1'b0;
+            remover = 1'b1;
+          end
+          default:
+          begin
+            avancar = 1'b0;
+            girar = 1'b1;
+            remover = 1'b0;
+          end
+        endcase
+      end
+      Acompanhando_Muro:
+      begin
+        casez({head,left,under,barrier})
+          4'b1000 , 4'b0000 , 4'b1100:
+          begin
+            avancar = 1'b0;
+            girar = 1'b1;
+            remover = 1'b0;
+          end
+          4'b??1?:
+          begin
+            avancar = 1'b0;
+            girar = 1'b0;
+            remover = 1'b0;
+          end
+          4'b0?01:
+          begin
+            avancar = 1'b0;
+            girar = 1'b0;
+            remover = 1'b1;
+          end
+          default:
+          begin
+            avancar = 1'b1;
+            girar = 1'b0;
+            remover = 1'b0;
+          end
+        endcase
+      end
+      Iniciando:
+      begin
+        casez({head,left,under,barrier})
+          4'b10?0 , 4'b11?0:
+          begin
+            avancar = 1'b0;
+            girar = 1'b1;
+            remover = 1'b0;
+          end
+          4'b0??0:
+          begin
+            avancar = 1'b1;
+            girar = 1'b0;
+            remover = 1'b0;
+          end
+          4'b0??1:
+          begin
+            avancar = 1'b0;
+            girar = 1'b0;
+            remover = 1'b1;
+          end
+        endcase
+      end
+      Removendo:
+      begin
+        casez ({head,left,under,barrier})
+          4'b0??1:
+          begin
+            avancar = 1'b0;
+            girar = 1'b0;
+            remover = 1'b1;
+          end
+          4'b0??0:
+          begin
+            avancar = 1'b1;
+            girar = 1'b0;
+            remover = 1'b0;
+          end
+        endcase
+      end
+      default:
+      begin
+        avancar = 1'b0;
+        girar = 1'b0;
+        remover = 1'b0;
+      end
+    endcase
+  end
+endmodule
+
